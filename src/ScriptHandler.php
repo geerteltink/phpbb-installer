f8ce4c54d11f2627b964aa90f216ab2846812dd9
Geert Eltink <xtreamwayz@gmail.com>
2015-01-26 17:32:32 +0100

    It really helps if a function is declared public and static when calling it statically

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -6,19 +6,19 @@ use Composer\Script\Event;
 
 class ScriptHandler
 {
-    public function postInstall(Event $event)
+    public static function postInstall(Event $event)
     {
         $composer = $event->getComposer();
     }
 
-    public function postUpdate(Event $event)
+    public static function postUpdate(Event $event)
     {
         $composer = $event->getComposer();
     }
 
     // Test composer scripts with:
     //      composer run-script post-install-cmd
-    private function install(Event $event)
+    public static function install(Event $event)
     {
         $composer = $event->getComposer();
         $eventName = $event->getName();
